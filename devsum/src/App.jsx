1b79b7ca813ceb6a916637aed7e2f2b319b2eb6c
Saad Shakeel <ultrasaad04@gmail.com>
2025-08-05 13:51:10 +0500

    Merger Ahsan-Mehar Branch

--- devsum/src/App.jsx
+++ devsum/src/App.jsx
@@ -14,6 +14,7 @@ import "./index.css";
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
+import Routes from "./routes/index";
 
 function App() {
   return (
@@ -24,6 +25,7 @@ function App() {
         <Route path="/" element={<HomePage />} />
          <Route path="/contact" element={<ContactUs />} />
           <Route path="/auth" element={<Auth />} />
+          
       </Routes>
       <Footer />
     </Router>
