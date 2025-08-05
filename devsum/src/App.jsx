c5c699e7464fa226fe7a8302475867eab396fd4e
Saad Shakeel <ultrasaad04@gmail.com>
2025-08-05 14:26:39 +0500

    Merged with Ahsan-Mehar Branch

--- devsum/src/App.jsx
+++ devsum/src/App.jsx
@@ -14,23 +14,34 @@ import "./index.css";
 import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
-import Routes from "./routes/index";
+import Coursespage from "./pages/CoursesPage";
+import ProjectDetail from "./pages/ProjectDetailsPage";
+import CoursesDetail from "./pages/CoursesDetailsPage";
+import ScrollUp from "./components/ScrollUp";
+import ScrollToTop from "./components/ScrollToTop";
+import ProjectsPage from "./pages/ProjectsPage";
 
 function App() {
-  return (
-    <>
-      <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-         <Route path="/contact" element={<ContactUs />} />
-          <Route path="/auth" element={<Auth />} />
-          
-      </Routes>
-      <Footer />
-    </Router>
-    </>
-  );
+ return (
+ <>
+    <Router>
+         <Navbar />
+         <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/contact" element={<ContactUs />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/courses" element={<Coursespage />} />
+            <Route path="/courses/:id" element={<CoursesDetail />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/projects/:id" element={<ProjectDetail />} />
+            <Route path="*" element={<NotFound />} /> 
+            </Routes>
+            <ScrollUp />
+            <Footer />
+      </Router>
+ </>
+);
 }
 
 export default App;
